844334cc3f2c7ebce58bf9c64740630224cce135
Arpon Purkayastha <arpon@arpons-mbp.lan>
2022-01-05 21:35:19 -0800

    pass in id into constructor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 /// <reference types="@types/google.maps" />
-
 // import { User } from './User';
 // import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+new CustomMap('map');
