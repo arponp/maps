44a070fdd1f0db88a9a0e3afe03c997ca036ae94
arponp <arponp30@gmail.com>
2022-08-09 22:29:00 -0700

    add Company

--- src/Company.ts
+++ src/Company.ts
@@ -1,14 +1,16 @@
 import { faker } from '@faker-js/faker';
 
-export class User {
+export class Company {
   name: string;
+  catchPharse: string;
   location: {
     lat: number;
     lng: number;
   };
 
   constructor() {
-    this.name = faker.name.firstName();
+    this.name = faker.company.name();
+    this.catchPharse = faker.company.catchPhrase();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
